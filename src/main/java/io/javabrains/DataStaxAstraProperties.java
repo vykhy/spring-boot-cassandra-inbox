e2e40ee1d2c56cdc492e0942b47f197d6dd7f643
vykhy <vikehietoyhoshii@gmail.com>
2023-07-09 11:07:01 +0530

    connect to astra DB

---
+++ src/main/java/io/javabrains/DataStaxAstraProperties.java
@@ -0,0 +1,20 @@
+package io.javabrains;
+
+import java.io.File;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "datastax.astra")
+public class DataStaxAstraProperties {
+
+    private File secureConnectBundle;
+    
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+}
--- src/main/java/io/javabrains/InboxApp.java
+++ src/main/java/io/javabrains/InboxApp.java
@@ -1,7 +1,11 @@
 package io.javabrains;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,10 @@ public class InboxApp {
 		SpringApplication.run(InboxApp.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 	
 
