dbbb445d0fd59ec28b9c8821829566482fe28d20
vykhy <vikehietoyhoshii@gmail.com>
2023-07-13 16:16:38 +0530

    mark as read on open; unread ui

--- src/main/java/io/javabrains/inbox/controllers/EmailViewController.java
+++ src/main/java/io/javabrains/inbox/controllers/EmailViewController.java
@@ -12,12 +12,17 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.javabrains.inbox.email.Email;
 import io.javabrains.inbox.email.EmailRepository;
+import io.javabrains.inbox.emaillist.EmailListItem;
+import io.javabrains.inbox.emaillist.EmailListItemKey;
+import io.javabrains.inbox.emaillist.EmailListItemRepository;
 import io.javabrains.inbox.folders.Folder;
 import io.javabrains.inbox.folders.FolderRepository;
 import io.javabrains.inbox.folders.FolderService;
+import io.javabrains.inbox.folders.UnreadEmailStatsRepository;
 
 @Controller
 public class EmailViewController {
@@ -28,9 +33,14 @@ public class EmailViewController {
     private FolderService folderService;
     @Autowired
     private EmailRepository emailRepository;
+    @Autowired
+    EmailListItemRepository emailListItemRepository;
+    @Autowired
+    UnreadEmailStatsRepository unreadEmailStatsRepository;
 
     @GetMapping("/emails/{id}")
     public String emailView(
+            @RequestParam String folder,
             @PathVariable UUID id,
             @AuthenticationPrincipal OAuth2User principal,
             Model model) {
@@ -44,18 +54,35 @@ public class EmailViewController {
         model.addAttribute("userFolders", userFolders);
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
-        model.addAttribute("stats", folderService.mapCountToLabels(userId));
 
         // Fetch email
         Optional<Email> optionalEmail = emailRepository.findById(id);
         if (!optionalEmail.isPresent()) {
-
+            model.addAttribute("stats", folderService.mapCountToLabels(userId));
             return "inbox-page";
         }
         Email email = optionalEmail.get();
         String toIds = String.join(", ", email.getTo());
         model.addAttribute("email", email);
         model.addAttribute("toIds", toIds);
+
+        EmailListItemKey key = new EmailListItemKey();
+        key.setId(userId);
+        key.setLabel(folder);
+        key.setTimeUUID(email.getId());
+
+        Optional<EmailListItem> optionalEmailListItem = emailListItemRepository.findById(key);
+        if (optionalEmail.isPresent()) {
+            EmailListItem emailListItem = optionalEmailListItem.get();
+            if (emailListItem.getIsUnread()) {
+                emailListItem.setIsUnread(false);
+                emailListItemRepository.save(emailListItem);
+
+                unreadEmailStatsRepository.decrementUnreadCounter(userId, folder);
+            }
+        }
+        model.addAttribute("stats", folderService.mapCountToLabels(userId));
+
         return "email-page";
     }
 }
